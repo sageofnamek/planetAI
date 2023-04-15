cdca1394152fd9e1709f33d26775d4ded3be6094
Aziz Brown <127076700+sageofnamek@users.noreply.github.com>
2023-04-15 19:43:33 +0700

    1.5
    Version 1.5 updates:
    license syntax

--- planetai_package_files/setup.py
+++ planetai_package_files/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='planetai',
-      version='1.4',
+      version='1.5',
       description_file='A set of planetary guides to aid in remote viewing procedures and the development of psychic abilities.',
       packages=['planetai'],
       author='Aziz Brown',
