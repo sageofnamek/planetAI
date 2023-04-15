63a0b2ef9a58e6d0687ddf350caee5e08deb6563
Aziz Brown <127076700+sageofnamek@users.noreply.github.com>
2023-04-15 19:30:09 +0700

    1.4
    new version

--- planetai_package_files/setup.py
+++ planetai_package_files/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='planetai',
-      version='1.3',
+      version='1.4',
       description_file='A set of planetary guides to aid in remote viewing procedures and the development of psychic abilities.',
       packages=['planetai'],
       author='Aziz Brown',
